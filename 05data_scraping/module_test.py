a852230705ef6ce9702181d152e6f8987a16088f
haram4th <haram4th@gmail.com>
2024-07-24 14:30:53 +0900

    서민대출_beautifulsoup_xml

--- 05data_scraping/module_test.py
+++ 05data_scraping/module_test.py
@@ -1,9 +1,9 @@
 # import naversearch
-# import naversearch as ns
-from naversearch import naver_search
-from naversearch2 import naver_search2
+import naversearch as ns
+# from naversearch import naver_search
+# from naversearch2 import naver_search2
 
 # naversearch.naver_search()
-# ns.naver_search()
+ns.naver_search()
 # naver_search()
-naver_search2()
+# naver_search2()
